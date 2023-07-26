8df90a087f65191226af4ef36fcd06f57fe4494c
Aravind V V <arvndvv@gmail.com>
2023-07-26 22:03:08 +0530

    update guards

--- src/guards/AuthGuard.tsx
+++ src/guards/AuthGuard.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useGlobalState } from "../global/store";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { toaster } from "../utils/toaster";
 import { WriteRoutes } from "../constants/routes";
 interface IAuthGuardProps {
@@ -9,26 +9,18 @@ interface IAuthGuardProps {
 
 export const AuthGuard: (props: IAuthGuardProps) => any = ({ component }) => {
   const { user } = useGlobalState();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (!user) {
-      toaster.error("You are not authenticated.");
-      navigate(WriteRoutes.SIGN_IN);
-    }
-  }, [navigate, user]);
-
+  if (!user) {
+    toaster.error("You are not authenticated.");
+    return <Navigate to={WriteRoutes.SIGN_IN} />;
+  }
   return <>{component}</>;
 };
 
 export const UnAuthGuard: (props: IAuthGuardProps) => any = ({ component }) => {
   const { user } = useGlobalState();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (user) {
-      toaster.success("You are authenticated.");
-      navigate(WriteRoutes.HOME);
-    }
-  }, [navigate, user]);
-
+  if (user) {
+    toaster.success("You are authenticated.");
+    return <Navigate to={WriteRoutes.HOME} />;
+  }
   return <>{component}</>;
 };
